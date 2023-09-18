7f824319fbbbe9482b6dc9e1b63b58e0c95c628a
Alexandre Thomassin <alex45thomassin@gmail.com>
2023-09-18 08:15:00 +0200

    #Initial commit

---
+++ World_of_ECN/src/main/java/edu/centrale/world_of_ecn/World_of_ECN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.centrale.world_of_ecn;
+
+/**
+ *
+ * @author alex4
+ */
+public class World_of_ECN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
