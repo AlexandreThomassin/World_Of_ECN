64039fa1e4bd9ec0967ac4e838e8a88caf693a9d
Alexandre Thomassin <alex45thomassin@gmail.com>
2023-10-12 08:52:28 +0200

    Implement Nourriture.java

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Nourriture.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Nourriture.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.centrale.objet.WoE;
+import java.lang.reflect.Field;
 
 /**
  *
@@ -16,10 +17,63 @@ public class Nourriture extends Objet implements Utilisable {
     
     private boolean isActive;
     
+    private String attrChanged;
+
+    public Nourriture(Integer duree, Integer value, String attrChanged) {
+        this.duree = duree;
+        this.value = value;
+        this.attrChanged = attrChanged;
+        this.isActive = false;
+    }
+    
+    public Nourriture(Nourriture n){
+        this.duree = n.duree;
+        this.value = n.value;
+        this.attrChanged = n.attrChanged;
+        this.isActive = n.isActive;
+    }
     
     public void utilise(){
         if (!isActive){
             isActive = true;
+            
+            try {
+                Field attribut = getClass().getDeclaredField("degAtt");
+                attribut.setAccessible(true);
+                try {
+                    
+                    attribut.setInt(this, attribut.getInt(this) + value);
+                    
+                } catch (IllegalAccessException e) {
+                    System.out.println("L'attribut " + attrChanged + " n'est pas accessible !");
+                }
+                
+            } catch (NoSuchFieldException e){
+                System.out.println("L'attribut " + attrChanged + " n'existe pas !");
+            }
+            
+            
+        } else {
+            // On décréemente la durée à chaque utilisation
+            duree = duree - 1;
+            
+            // Si la durée arrive à 0 on inverse les effets de la nourriture pour revenir à la normale
+            if (duree == 0){
+                try {
+                Field attribut = getClass().getDeclaredField("degAtt");
+                attribut.setAccessible(true);
+                try {
+                    
+                    attribut.setInt(this, attribut.getInt(this) - value);
+                    
+                } catch (IllegalAccessException e) {
+                    System.out.println("L'attribut " + attrChanged + " n'est pas accessible !");
+                }
+                
+            } catch (NoSuchFieldException e){
+                System.out.println("L'attribut " + attrChanged + " n'existe pas !");
+            }
+            }
         }
     }
     
